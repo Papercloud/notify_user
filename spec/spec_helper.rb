7df2c27225655737499c608a239691082177291d
Tomas Spacek <tomspacek@gmail.com>
2013-12-13 18:46:28 +1100

    Account for Travis using absolute gemfile paths

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH << File.expand_path('../support', __FILE__)
 
+# Use Rails 4 by default if you just do 'rspec spec'
 ENV['BUNDLE_GEMFILE'] ||= 'gemfiles/rails40.gemfile'
 
-ENV['BUNDLE_GEMFILE'] = File.expand_path(File.join("../../", ENV['BUNDLE_GEMFILE']), __FILE__)
+ENV['BUNDLE_GEMFILE'] = File.expand_path(ENV['BUNDLE_GEMFILE'])
 require "bundler"
 Bundler.setup
 
