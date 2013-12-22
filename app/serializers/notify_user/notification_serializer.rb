75f8faf95682ea640647d18b68d2452c6444b81e
Tomas Spacek <tomspacek@gmail.com>
2013-12-23 00:51:49 +1100

    Add 'read' field to JSON serialized message

--- app/serializers/notify_user/notification_serializer.rb
+++ app/serializers/notify_user/notification_serializer.rb
@@ -1,22 +1,15 @@
 class NotifyUser::NotificationSerializer < ActiveModel::Serializer
   root :notifications
 
-  attributes :id, :message
+  attributes :id, :message, :read
 
   def message
     options[:template_renderer].render_to_string(:template => object.class.views[:mobile_sdk][:template_path].call(object),
                                                  :locals => {params: object.params},
                                                  :layout => false)
   end
+
+  def read
+    object.read?
+  end
 end
-
-
-# How does the message for JSON work?
-# It's just another channel?
-# But then how does the message for web work? Another channel again?
-# I think I'd rather have just one controller.
-# Just need a view to override and render
-# Or maybe send the templates down with the data separately, then join on the client.
-# Still need to be able to use templats eventually for things like APNS though.
-
-# So we can render a template...just need to decide where to put it.
--- spec/serializers/notify_user/notification_serializer_spec.rb
+++ spec/serializers/notify_user/notification_serializer_spec.rb
@@ -5,6 +5,6 @@ describe NotifyUser::NotificationSerializer do
   it { should include_root(:notifications) }
   it { should have_attribute(:message) }
   it { should have_attribute(:id) }
-  it { pending "implement unread/read state"}  #should have_attribute(:read)
+  it { should have_attribute(:read) }
   
 end
