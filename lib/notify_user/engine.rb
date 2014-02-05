965df15f28ef4fe59d692bf9348ae932b466d7be
William Porter <dawilster143@gmail.com>
2014-02-05 14:02:03 +1100

    fix serializer issue

--- lib/notify_user/engine.rb
+++ lib/notify_user/engine.rb
@@ -1,5 +1,6 @@
 module NotifyUser
   class Engine < ::Rails::Engine
+  	require 'active_model_serializers'
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
