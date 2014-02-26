9f9958c10379bd779411916e13e27f615f317f1f
William Porter <dawilster143@gmail.com>
2014-02-26 12:03:14 +1100

    syntax fix in email layout

--- app/helpers/notify_user/application_helper.rb
+++ app/helpers/notify_user/application_helper.rb
@@ -5,5 +5,9 @@ module NotifyUser
   		html = link_to(text, notify_user_notifications_unauth_unsubscribe_url(:type => notification.type, :token => user_hash.token))
   		return html.to_s.html_safe
   	end
+
+  	def is_unsubscribeable?(notification)
+  		return NotifyUser.unsubscribable_notifications.include? notification.type
+  	end
   end
 end
