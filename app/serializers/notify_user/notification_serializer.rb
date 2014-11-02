401c920e7d23d4cede7a8a3700d10db7dfefe15d
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2014-11-03 10:32:31 +1100

    Removed the generator for the `mobile_sdk` folder and template, as well as the `message` method that is defined within the serialiser.

--- app/serializers/notify_user/notification_serializer.rb
+++ app/serializers/notify_user/notification_serializer.rb
@@ -3,15 +3,6 @@ class NotifyUser::NotificationSerializer < ActiveModel::Serializer
 
   attributes :id, :type, :message, :read, :params, :created_at
 
-  def message
-    ActionController::Base.new.render_to_string(
-      template: object.class.views[:mobile_sdk][:template_path].call(object),
-      locals: {params: object.params},
-      layout: false,
-      formats: [:html]
-    )
-  end
-
   def read
     object.read?
   end
--- lib/generators/notify_user/notification/notification_generator.rb
+++ lib/generators/notify_user/notification/notification_generator.rb
@@ -11,7 +11,6 @@ module NotifyUser
 
     def generate_view_scaffolds
       template "email_template.html.erb.erb", "app/views/notify_user/#{name.underscore}/action_mailer/notification.html.erb"
-      template "mobile_sdk_template.html.erb.erb", "app/views/notify_user/#{name.underscore}/mobile_sdk/notification.html.erb"
       template "email_layout_template.html.erb.erb", "app/views/notify_user/layouts/action_mailer.html.erb"
     end
   end
