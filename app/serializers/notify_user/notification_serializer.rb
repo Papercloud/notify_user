12739999a7bef26d62422b0046930f18b662e81a
William Porter <dawilster143@gmail.com>
2014-12-03 15:44:57 +1100

    updated so that notification message can include htmlentities and have them properly decoded before being outputted to json

--- app/serializers/notify_user/notification_serializer.rb
+++ app/serializers/notify_user/notification_serializer.rb
@@ -1,12 +1,14 @@
 class NotifyUser::NotificationSerializer < ActiveModel::Serializer
+  require 'cgi'
   root :notifications
 
   attributes :id, :type, :message, :read, :params, :created_at
 
   def message
-    options[:template_renderer].render_to_string(:template => object.class.views[:mobile_sdk][:template_path].call(object),
+    string = options[:template_renderer].render_to_string(:template => object.class.views[:mobile_sdk][:template_path].call(object),
                                                  :locals => {params: object.params},
                                                  :layout => false, :formats => [:html])
+    return ::CGI.unescapeHTML("#{string}")
   end
 
   def read
