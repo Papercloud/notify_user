478fa06adf14169e3b8f75bda674af6a52c67700
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-01-09 15:41:22 +1100

    Changed the `apns_channel` class to now do some setup based on the `apns_provider` that's been set in the initialiser.

--- lib/notify_user/channels/apns/apns_channel.rb
+++ lib/notify_user/channels/apns/apns_channel.rb
@@ -9,22 +9,25 @@ class ApnsChannel
   	end
 
     def deliver(notification, options={})
-      NotifyUser::Apns.push_notification(notification)
+      case NotifyUser.apns_provider
+      when :houston
+        NotifyUser::Houston.new(notification).push
+      when :urban_airship
+        # Check for the existence of development api keys and resend for development:
+        if !ENV['DEV_UA_APPLICATION_KEY'].nil? && !ENV['DEV_UA_APPLICATION_SECRET'].nil? && !ENV['DEV_UA_MASTER_SECRET'].nil?
 
-      #check for the existence of development api keys and resend for development
-      if !ENV['DEV_UA_APPLICATION_KEY'].nil? && !ENV['DEV_UA_APPLICATION_SECRET'].nil? && !ENV['DEV_UA_MASTER_SECRET'].nil?
+          Urbanairship.application_key = ENV['DEV_UA_APPLICATION_KEY']
+          Urbanairship.application_secret = ENV['DEV_UA_APPLICATION_SECRET']
+          Urbanairship.master_secret = ENV['DEV_UA_MASTER_SECRET']
 
-        Urbanairship.application_key = ENV['DEV_UA_APPLICATION_KEY']
-        Urbanairship.application_secret = ENV['DEV_UA_APPLICATION_SECRET']
-        Urbanairship.master_secret = ENV['DEV_UA_MASTER_SECRET']
+          NotifyUser::UrbanAirship.new(notification).push
 
-        NotifyUser::Apns.push_notification(notification)
+          # Sets the api keys back to their original state:
 
-        #sets the api keys back to their original state
-
-        Urbanairship.application_key = ENV['UA_APPLICATION_KEY']
-        Urbanairship.application_secret = ENV['UA_APPLICATION_SECRET']
-        Urbanairship.master_secret = ENV['UA_MASTER_SECRET']
+          Urbanairship.application_key = ENV['UA_APPLICATION_KEY']
+          Urbanairship.application_secret = ENV['UA_APPLICATION_SECRET']
+          Urbanairship.master_secret = ENV['UA_MASTER_SECRET']
+        end
       end
     end
 
