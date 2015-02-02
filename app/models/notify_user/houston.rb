7a75dc9f50106b3e22b1288417b59195948d7ff2
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-02-03 09:53:15 +1100

    Restructured a lot of how the Houston notifications are working right now, the now retry sending the broken notification a number of times until it either succeeds or fails completely and is removed from the database. Biggest change though is that the connections are only re-spawned if an error is encountered (we close the connection manually), instead of a connection being opened every run through.
    Hoping to play with this more and get something going similar to what's mentioned [here](https://github.com/nomad/houston/issues/83), and have one thread for reading the socket, and the main thread is just used for writing.

--- app/models/notify_user/houston.rb
+++ app/models/notify_user/houston.rb
@@ -4,70 +4,77 @@ require 'houston'
 module NotifyUser
   class Houston < Apns
 
+    NO_ERROR = -42
     APN_POOL = ConnectionPool.new(
       size: NotifyUser.connection_pool_size,
       timeout: NotifyUser.connection_pool_timeout) do
       APNConnection.new
     end
 
-    NO_ERROR = -42
+    def initialize(notification, options)
+      super(notification, options)
+
+      @devices = @notification.target.devices.order(:id).reverse
+    end
 
     def push
       space_allowance = PAYLOAD_LIMIT - used_space
 
-      push_options = {
+      @push_options = {
         alert: @notification.mobile_message(space_allowance),
         badge: @notification.count_for_target,
         category: @notification.params[:category] || @notification.type,
         custom_data: @notification.params
       }
 
-      Houston.notify(push_options, @notification.target.devices)
+      send_notifications
     end
 
-    class << self
-
-      def notify(push_options = {}, devices)
-        APN_POOL.with do |connection|
+    def send_notifications
+      APN_POOL.with do |connection|
+        if !connection.connection.open?
           connection = APNConnection.new
+        end
 
-          ssl = connection.ssl
-          error_index = NO_ERROR
+        ssl = connection.ssl
+        error_index = NO_ERROR
 
-          devices.each_with_index do |device, index|
-            notification = ::Houston::Notification.new(push_options.dup.merge({ token: device.token, id: index }))
+        @devices.each_with_index do |device, index|
+          notification = ::Houston::Notification.new(@push_options.dup.merge({ token: device.token, id: index }))
 
-            connection.write(notification.message)
-          end
+          connection.write(notification.message)
+        end
 
-          read_socket, write_socket = IO.select([ssl], [], [ssl], 1)
-          if (read_socket && read_socket[0])
-            if error = connection.connection.read(6)
-              command, status, error_index = error.unpack("ccN")
+        read_socket, write_socket = IO.select([ssl], [], [ssl], 1)
+        if (read_socket && read_socket[0])
+          if error = connection.connection.read(6)
+            command, status, error_index = error.unpack("ccN")
 
-              if error_index != NO_ERROR
-                device = devices[error_index]
+            if error_index != NO_ERROR
+              device = @devices[error_index]
+              range = 0...error_index
 
-                if device.failures + 1 > NotifyUser.failure_tolerance
-                  # Delete the device token if it has failed consecutively too many times:
-                  device.delete
-                else
-                  # Up the failure rate for the device that failed to send:
-                  device.update_attributes(failures: device.failures + 1)
-                end
+              if device.failures + 1 >= NotifyUser.failure_tolerance
+                # Delete the device token if it has failed consecutively too many times:
+                device.delete
+                range = 0..error_index
+              else
+                # Up the failure rate for the device that failed to send:
+                device.update_attributes(failures: device.failures + 1)
               end
+
+              # Close the connection:
+              connection.connection.close
+
+              # Set the failure rate for all successfully sent notifications back to 0:
+              Device.where(id: @devices.slice!(range).map(&:id)).update_all(failures: 0 )
             end
           end
-
-          # Set the failure rate for all successfully sent notifications back to 0:
-          Device.where(id: devices[0...error_index].map(&:id)).update_all(failures: 0 )
-
-          # Resend all notifications after the once that produced the error:
-          notify(push_options, devices[(error_index+1)..-1]) if error_index != NO_ERROR
         end
+
+        # Resend all notifications after the once that produced the error:
+        send_notifications if error_index != NO_ERROR
       end
-
     end
-
   end
 end
