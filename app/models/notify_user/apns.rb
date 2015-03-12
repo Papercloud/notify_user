2061262d233f756f9b376c25ee847833c9073acf
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-03-12 11:29:42 +1100

    Moved the `used_space` method into the list of private methods for the `APNS` class.

--- app/models/notify_user/apns.rb
+++ app/models/notify_user/apns.rb
@@ -8,6 +8,13 @@ module NotifyUser
       @options = options
     end
 
+    # Sends push notification:
+    def push
+      raise "Base APNS class should not be used."
+    end
+
+    private
+
     # Calculates the bytes already used:
     def used_space
       used_space = SYMBOL_NAMES_SIZE + @notification.id.size + @notification.created_at.to_time.to_i.size +
@@ -16,11 +23,6 @@ module NotifyUser
       used_space += @notification.params[:action_id].size if @notification.params[:action_id]
 
       used_space
-    end
-
-    # Sends push notification:
-    def push
-      raise "Base APNS class should not be used."
     end
   end
 end
