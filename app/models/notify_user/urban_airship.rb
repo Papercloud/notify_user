09f2879e2ea1f99836b404f55a75990e697537ce
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-07-23 16:22:10 +1000

    Temporarily removes more `urbanairship` stuff.

--- app/models/notify_user/urban_airship.rb
+++ app/models/notify_user/urban_airship.rb
@@ -1,33 +1,33 @@
 module NotifyUser
   class UrbanAirship < Apns
 
-    def push
-      space_allowance = PAYLOAD_LIMIT - used_space
+    # def push
+    #   space_allowance = PAYLOAD_LIMIT - used_space
 
-      payload = {
-        :alias  => notification.target_id,
-        :aps    => {
-          :alert => notification.mobile_message(space_allowance),
-          :badge => notification.count_for_target
-        },
-        :n_data => {
-          '#' => notification.id,
-          :t  => notification.created_at.to_time.to_i,
-          '?' => notification.type
-        }
-      }
+    #   payload = {
+    #     :alias  => notification.target_id,
+    #     :aps    => {
+    #       :alert => notification.mobile_message(space_allowance),
+    #       :badge => notification.count_for_target
+    #     },
+    #     :n_data => {
+    #       '#' => notification.id,
+    #       :t  => notification.created_at.to_time.to_i,
+    #       '?' => notification.type
+    #     }
+    #   }
 
-      payload[:n_data]['!'] = notification.params[:action_id] if notification.params[:action_id]
+    #   payload[:n_data]['!'] = notification.params[:action_id] if notification.params[:action_id]
 
-      response = Urbanairship.push(payload)
-      if response.success?
-        Rails.logger.info "Push notification sent successfully."
-        return true
-      else
-        Rails.logger.info "Push notification failed."
-        return false
-      end
-    end
+    #   response = Urbanairship.push(payload)
+    #   if response.success?
+    #     Rails.logger.info "Push notification sent successfully."
+    #     return true
+    #   else
+    #     Rails.logger.info "Push notification failed."
+    #     return false
+    #   end
+    # end
 
   end
 end
