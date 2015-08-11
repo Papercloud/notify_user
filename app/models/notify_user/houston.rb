caaf5ded2640c5aeca6558a2e607e7a759c64f53
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-08-11 23:46:17 +1000

    Restructure of the connection to APNS; rather than maintain a connection pool, just maintain the one and and reset it correctly if we hit a timeout, etc.

--- app/models/notify_user/houston.rb
+++ app/models/notify_user/houston.rb
@@ -6,11 +6,6 @@ module NotifyUser
 
     NO_ERROR = -42
     INVALID_TOKEN_ERROR = 8
-    APN_POOL = ConnectionPool.new(
-      size: NotifyUser.connection_pool_size,
-      timeout: NotifyUser.connection_pool_timeout) do
-      APNConnection.new
-    end
 
     attr_accessor :push_options
 
@@ -32,6 +27,16 @@ module NotifyUser
     end
 
     private
+
+    def connection
+      @connection ||= APNConnection.new
+    end
+
+    def reset_connection
+      @connection.connection.close if @connection
+      @connection = nil
+      connection
+    end
 
     def setup_options
       space_allowance = PAYLOAD_LIMIT - used_space
@@ -67,61 +72,60 @@ module NotifyUser
     end
 
     def send_notifications
-      APN_POOL.with do |connection|
-        Rails.logger.info "PAYLOAD"
-        Rails.logger.info "----"
-        Rails.logger.info "#{@push_options}"
+      Rails.logger.info "PAYLOAD"
+      Rails.logger.info "----"
+      Rails.logger.info "#{@push_options}"
 
-        unless valid?(@push_options)
-          Rails.logger.info "Error: Payload exceeds size limit."
-        end
+      unless valid?(@push_options)
+        Rails.logger.info "Error: Payload exceeds size limit."
+      end
 
-        if connection.connection.closed?
-          connection = APNConnection.new
-        end
+      ssl = connection.connection.ssl
+      error_index = NO_ERROR
 
-        ssl = connection.connection.ssl
-        error_index = NO_ERROR
+      @devices.each_with_index do |device, index|
+        notification = ::Houston::Notification.new(@push_options.dup.merge({ token: device.token, id: index }))
+        connection.write(notification.message)
+      end
 
-        @devices.each_with_index do |device, index|
-          notification = ::Houston::Notification.new(@push_options.dup.merge({ token: device.token, id: index }))
-          connection.write(notification.message)
-        end
+      Rails.logger.info "READING ERRORS"
+      Rails.logger.info "----"
+      read_socket, write_socket = IO.select([ssl], [], [ssl], 1)
+      Rails.logger.info "#{ssl}"
 
-        Rails.logger.info "READING ERRORS"
-        Rails.logger.info "----"
-        read_socket, write_socket = IO.select([ssl], [], [ssl], 1)
-        Rails.logger.info "#{ssl}"
+      if (read_socket && read_socket[0])
+        error = connection.connection.read(6)
 
-        if (read_socket && read_socket[0])
-          error = connection.connection.read(6)
+        Rails.logger.info "#{error}"
 
-          Rails.logger.info "#{error}"
+        if error
+          command, status, error_index = error.unpack("ccN")
 
-          if error
-            command, status, error_index = error.unpack("ccN")
+          Rails.logger.info "Error: #{status} with id: #{error_index}."
 
-            Rails.logger.info "Error: #{status} with id: #{error_index}."
+          # Remove all the devices prior to the error (we assume they were successful), and close the current connection:
+          if error_index != NO_ERROR
+            device = @devices.at(error_index)
 
-            # Remove all the devices prior to the error (we assume they were successful), and close the current connection:
-            if error_index != NO_ERROR
-              device = @devices.at(error_index)
-
-              # If we encounter the Invalid Token error from APNS, just remove the device:
-              if status == INVALID_TOKEN_ERROR
-                Rails.logger.info "Invalid token encountered, removing device. Token: #{device.token}."
-                device.destroy
-              end
-
-              @devices.slice!(0..error_index)
-              connection.connection.close
+            # If we encounter the Invalid Token error from APNS, just remove the device:
+            if status == INVALID_TOKEN_ERROR
+              Rails.logger.info "Invalid token encountered, removing device. Token: #{device.token}."
+              device.destroy
             end
+
+            @devices.slice!(0..error_index)
+            reset_connection
           end
         end
-
-        # Resend all notifications after the once that produced the error:
-        send_notifications if error_index != NO_ERROR
       end
+
+      # Resend all notifications after the once that produced the error:
+      send_notifications if error_index != NO_ERROR
+    rescue OpenSSL::SSL::SSLError, Errno::EPIPE, Errno::ETIMEDOUT => e
+      # Rails.loggere, "[##{connection.object_id}] Exception occurred: #{e.inspect}, connection state: #{connection.inspect}")
+      reset_connection
+      # APN.log(:debug, "[##{connection.object_id}] Socket reestablished, connection state: #{connection.inspect}")
+      retry
     end
   end
 end
