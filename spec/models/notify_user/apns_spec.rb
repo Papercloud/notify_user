f3e8916ef874c59f1e0b919ce28e99cca06b1957
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-09-07 13:00:04 +1000

    Renames the houston specs to be apns specs.

--- spec/models/notify_user/apns_spec.rb
+++ spec/models/notify_user/apns_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 module NotifyUser
-  describe Houston do
+  describe Apns do
 
     let(:user) { User.create({email: 'user@example.com' })}
     let(:notification) { NewPostNotification.create({target: user}) }
@@ -13,38 +13,38 @@ module NotifyUser
 
     describe "initialisation" do
       it "should initialise the correct push options" do
-        @houston = NotifyUser::Houston.new([notification], {})
+        @apns = NotifyUser::Apns.new([notification], {})
 
-        expect(@houston.push_options[:alert]).to eq 'New Notification'
-        expect(@houston.push_options[:badge]).to eq 1
-        expect(@houston.push_options[:category]).to eq 'NewPostNotification'
-        expect(@houston.push_options[:custom_data]).to be_empty
-        expect(@houston.push_options[:sound]).to eq 'default'
+        expect(@apns.push_options[:alert]).to eq 'New Notification'
+        expect(@apns.push_options[:badge]).to eq 1
+        expect(@apns.push_options[:category]).to eq 'NewPostNotification'
+        expect(@apns.push_options[:custom_data]).to be_empty
+        expect(@apns.push_options[:sound]).to eq 'default'
       end
 
       it 'should access the notification targets list of devices' do
         expect(user).to receive(:devices)
 
-        @houston = NotifyUser::Houston.new([notification], {})
+        @apns = NotifyUser::Apns.new([notification], {})
       end
 
       it 'should use the sound specified in the options' do
-        @houston = NotifyUser::Houston.new([notification], { sound: 'special.wav' })
+        @apns = NotifyUser::Apns.new([notification], { sound: 'special.wav' })
 
-        expect(@houston.push_options[:sound]).to eq 'special.wav'
+        expect(@apns.push_options[:sound]).to eq 'special.wav'
       end
 
       it 'should remove the badge key for silent notifications' do
-        @houston = NotifyUser::Houston.new([notification], { silent: true })
+        @apns = NotifyUser::Apns.new([notification], { silent: true })
 
-        expect(@houston.push_options).not_to have_key(:badge)
+        expect(@apns.push_options).not_to have_key(:badge)
       end
 
       xit "should initialize with many notifications" do
         expect(NotifyUser::BaseNotification).to receive(:aggregate_message).and_return("New Notification")
         notifications = NewPostNotification.create([{target: user}, {target: user}, {target: user}])
 
-        NotifyUser::Houston.new(notifications, {})
+        NotifyUser::Apns.new(notifications, {})
       end
     end
 
