82379a87d3490c391fdf5ef80c589d6115ec394c
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-09-07 15:30:48 +1000

    Fixes issue of options not being passed through.
    Kind of ugly, but makes it so that the device method is what is passed through over the actual options.

--- lib/notify_user/channels/apns/apns_channel.rb
+++ lib/notify_user/channels/apns/apns_channel.rb
@@ -9,14 +9,14 @@ class ApnsChannel
   	end
 
     def deliver(notification, options={})
-      @devices = fetch_devices(notification)
+      @devices = fetch_devices(notification, options[:device_method])
 
       NotifyUser::Apns.new([notification], @devices[:ios], options).push if @devices[:ios].any?
       NotifyUser::Gcm.new([notification], @devices[:android], options).push if @devices[:android].any?
     end
 
     def deliver_aggregated(notifications, options={})
-      @devices = fetch_devices(notification)
+      @devices = fetch_devices(notification, options[:device_method])
 
       NotifyUser::Apns.new(notifications, @devices[:ios], options).push if @devices[:ios].any?
       NotifyUser::Gcm.new(notifications, @devices[:android], options).push if @devices[:android].any?
@@ -24,8 +24,7 @@ class ApnsChannel
 
     private
 
-    def fetch_devices(notification)
-      device_method = options[:device_method] || :devices
+    def fetch_devices(notifications, device_method = :devices)
       devices = notification.target.send(device_method)
 
       { ios: devices.ios, android: devices.android }
