0d1fd5fd902f086177a7b97ce3629b384061e227
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2017-04-11 13:03:24 +1000

    Rewrites GCM class to handle logging and responses in the same way APNS does

--- app/models/notify_user/gcm.rb
+++ app/models/notify_user/gcm.rb
@@ -4,15 +4,11 @@ module NotifyUser
   class Gcm < Push
     PAYLOAD_LIMIT = 4096
 
-    attr_accessor :client, :push_options
-
-    def initialize(notifications, devices, options)
-      super(notifications, devices, options)
-    end
-
     def push
       send_notifications
     end
+
+    private
 
     def client
       @client ||= GCM.new(ENV['GCM_API_KEY'])
@@ -22,19 +18,36 @@ module NotifyUser
       payload.to_json.bytesize <= PAYLOAD_LIMIT
     end
 
-    private
+    def send_notifications
+      device = fetch_device(delivery.notification, delivery.device_token)
+      fail "Device not registered for target" unless device
 
-    def build_notification
-      return Factories::Gcm.build(@notification, @options)
+      notification_data = build_notification(delivery.notification)
+      response = client.send(device.token, notification_data)
+
+      log_response_to_delivery(response)
+      handle_response(response, device)
     end
 
-    def send_notifications
-      return unless device_tokens.any?
-      notification_data = build_notification()
+    def fetch_device(notification, device_token)
+      notification.target.devices.find_by(token: device_token)
+    end
 
-      response = client.send(device_tokens, notification_data)
-      # should be checking for errors in the response here
-      return true
+    def build_notification(notification)
+      return Factories::Gcm.build(notification, options)
+    end
+
+    def log_response_to_delivery(response)
+      body = JSON.parse(response[:body])
+      delivery.update(status: response[:status_code], reason: body['results'][0]['error'])
+    end
+
+    def handle_response(response, device)
+      body = JSON.parse(response[:body])
+
+      if body['failure'] > 0 && (body['results'][0]['error'] == 'InvalidRegistration' || body['results'][0]['error'] == 'NotRegistered')
+        device.destroy
+      end
     end
   end
 end
